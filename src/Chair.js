081e10f2688046c88e2267f63a10d3422c7db57d
Jose Manuel <jmdcarq89@gmail.com>
2021-03-09 22:06:10 +0100

    domContent

--- src/Chair.js
+++ src/Chair.js
@@ -1,4 +1,4 @@
-import React, {Suspense, useRef} from 'react';
+import React, {Suspense, useRef, useEffect} from 'react';
 import "./Chair.scss";
 
 //Components
@@ -7,6 +7,9 @@ import {Section} from './components/section';
 import { Canvas, useFrame } from "react-three-fiber";
 
 import {Html, useGLTF} from "@react-three/drei";
+
+//page states
+import state from './components/state';
 
 const Model = ({modelPath}) => {
   const gltf = useGLTF(modelPath, true);
@@ -24,7 +27,7 @@ const Lights = () => {
   );
 }
 
-const HTMLContent = ({children, modelPath, positionY}) => {
+const HTMLContent = ({domContent, children, modelPath, positionY}) => {
 
   const ref = useRef();
   useFrame(() => (ref.current.rotation.y += 0.01));
@@ -35,31 +38,47 @@ const HTMLContent = ({children, modelPath, positionY}) => {
              <mesh ref={ref} position={[0,-35,0]}>
                <Model modelPath={modelPath}/>
              </mesh>
-           <Html fullscreen>{children}</Html> 
+           <Html portal={domContent} fullscreen>{children}</Html> 
            </group>
       </Section>
   );
 };
 
 export default function Chair() {
+
+  const domContent = useRef();
+  const scrollArea = useRef();
+  const onScroll = (e) => (state.top.current = e.target.scrollTop);
+  useEffect(() => void onScroll({target: scrollArea.current}), []);
+
+
   return (
     <>
       <Header/>
       <Canvas colorManagement camera={{ position: [0,0,120], fov:70}}>
         <Lights/>
           <Suspense fallback={null}>
-             <HTMLContent modelPath="/armchairYellow.gltf" positionY={250}>
+             <HTMLContent domContent={domContent} modelPath="/armchairYellow.gltf" positionY={250}>
              <div className='container'>
                 <h1 className='title'>Yellow</h1>
               </div>
              </HTMLContent>
-              <HTMLContent modelPath="/armchairGreen.gltf" positionY={0}>
+              <HTMLContent domContent={domContent} modelPath="/armchairGreen.gltf" positionY={0}>
              <div className='container'>
                 <h1 className='title'>Green</h1>
               </div>
              </HTMLContent>
+             <HTMLContent domContent={domContent} modelPath="/armchairGray.gltf" positionY={-250}>
+             <div className='container'>
+                <h1 className='title'>Gray</h1>
+              </div>
+             </HTMLContent>
           </Suspense>
       </Canvas>
+      <div className='scrollArea' ref={scrollArea} onScroll={onScroll}>
+        <div style={{position: 'sticky', top:0}} ref={domContent}></div>
+        <div style={{height: `${state.sections * 100}vh`}}></div>
+      </div>
     </>
   )
 }
