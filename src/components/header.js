a6341564e9efaca93591c114801bb8a1ff763564
Jose Manuel <jmdcarq89@gmail.com>
2021-03-09 22:50:34 +0100

    onechair fix

--- src/components/header.js
+++ src/components/header.js
@@ -6,23 +6,6 @@ export default function Header() {
       <div className='header-inner'>
         <div className='logo'>CHAIR.</div>
         <nav>
-          <ul>
-            <li>
-              <a href='/'>discover</a>
-            </li>
-            <li>
-              <a href='/'>products</a>
-            </li>
-            <li>
-              <a href='/'>solutions</a>
-            </li>
-            <li>
-              <a href='/'>reach</a>
-            </li>
-            <li className='btn'>
-              <a href='/'>order</a>
-            </li>
-          </ul>
         </nav>
       </div>
     </header>
