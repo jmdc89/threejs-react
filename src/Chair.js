74071e39ceb1cdda007bd7ed44c619ff4cc1b1f2
Jose Manuel <jmdcarq89@gmail.com>
2021-03-09 19:24:48 +0100

    model1 load

--- src/Chair.js
+++ src/Chair.js
@@ -1,3 +1,4 @@
+import React, {Suspense} from 'react';
 import "./Chair.scss";
 
 //Components
@@ -5,12 +6,20 @@ import Header from "./components/header";
 import {Section} from './components/section';
 import { Canvas } from "react-three-fiber";
 
-import {Html} from "@react-three/drei";
+import {Html, useGLTF} from "@react-three/drei";
+
+const Model = () => {
+  const gltf = useGLTF("/armchairYellow.gltf", true);
+  return <primitive object={gltf.scene} dispose={null} />;
+};
 
 const HTMLContent = () => {
   return (
       <Section factor={1.5} offset={1}>
            <group position={[0,250,0]}>
+             <mesh position={[0,35,0]}>
+               <Model/>
+             </mesh>
            <Html fullscreen>
               <div className='container'>
                 <h1 className='title'>Hello</h1>
@@ -26,7 +35,10 @@ export default function Chair() {
     <>
       <Header/>
       <Canvas colorManagement camera={{ position: [0,0,120], fov:70}}>
-          <HTMLContent/>
+          <Suspense fallback={null}>
+             <HTMLContent/>
+          </Suspense>
+
       </Canvas>
     </>
   )
